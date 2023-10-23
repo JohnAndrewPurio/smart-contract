75ca2bb15ffd0144d29df10ae46a2d55cbf135b6
Andrew Purio <lyerdestroyer@gmail.com>
2023-10-23 17:52:16 +0800

    Added donations

--- src/contract.rs
+++ src/contract.rs
@@ -3,7 +3,7 @@ use cosmwasm_std::{to_binary, Binary, Deps, DepsMut, Env, MessageInfo, Response,
 use crate::{
     error::ContractError,
     msg::{ExecuteMsg, GreetResp, InstantiateMsg, QueryMsg},
-    state::ADMINS,
+    state::{ADMINS, DONATION_DENOM},
 };
 
 pub fn instantiate(
@@ -18,6 +18,7 @@ pub fn instantiate(
         .map(|addr| deps.api.addr_validate(&addr))
         .collect();
     ADMINS.save(deps.storage, &admins?)?;
+    DONATION_DENOM.save(deps.storage, &msg.donation_denom)?;
 
     Ok(Response::new())
 }
@@ -42,6 +43,7 @@ pub fn execute(
     match msg {
         AddMembers { admins } => exec::add_members(deps, info, admins),
         Leave {} => exec::leave(deps, info).map_err(Into::into),
+        Donate {} => exec::donate(deps, info),
     }
 }
 
@@ -67,7 +69,7 @@ mod query {
 }
 
 mod exec {
-    use cosmwasm_std::Event;
+    use cosmwasm_std::{coins, BankMsg, Event};
 
     use super::*;
     use crate::error::ContractError;
@@ -115,6 +117,28 @@ mod exec {
 
         Ok(Response::new())
     }
+
+    pub fn donate(deps: DepsMut, info: MessageInfo) -> Result<Response, ContractError> {
+        let denom = DONATION_DENOM.load(deps.storage)?;
+        let admins = ADMINS.load(deps.storage)?;
+
+        let donation = cw_utils::must_pay(&info, &denom)?.u128();
+
+        let donation_per_admin = donation / (admins.len() as u128);
+
+        let messages = admins.into_iter().map(|admin| BankMsg::Send {
+            to_address: admin.to_string(),
+            amount: coins(donation_per_admin, &denom),
+        });
+
+        let resp = Response::new()
+            .add_messages(messages)
+            .add_attribute("action", "donate")
+            .add_attribute("amount", donation.to_string())
+            .add_attribute("per_admin", donation_per_admin.to_string());
+
+        Ok(resp)
+    }
 }
 
 #[cfg(test)]
@@ -122,8 +146,70 @@ mod tests {
     use crate::msg::AdminsListResp;
 
     use super::*;
-    use cosmwasm_std::Addr;
+    use cosmwasm_std::{coins, Addr};
     use cw_multi_test::{App, ContractWrapper, Executor};
+
+    #[test]
+    fn donations() {
+        let mut app = App::new(|router, _, storage| {
+            router
+                .bank
+                .init_balance(storage, &Addr::unchecked("user"), coins(5, "eth"))
+                .unwrap();
+        });
+
+        let code = ContractWrapper::new(execute, instantiate, query);
+        let code_id = app.store_code(Box::new(code));
+
+        let addr = app
+            .instantiate_contract(
+                code_id,
+                Addr::unchecked("owner"),
+                &InstantiateMsg {
+                    admins: vec!["admin1".to_owned(), "admin2".to_owned()],
+                    donation_denom: "eth".to_owned(),
+                },
+                &[],
+                "Contract",
+                None,
+            )
+            .unwrap();
+
+        app.execute_contract(
+            Addr::unchecked("user"),
+            addr.clone(),
+            &ExecuteMsg::Donate {},
+            &coins(5, "eth"),
+        )
+        .unwrap();
+
+        assert_eq!(
+            app.wrap()
+                .query_balance(&addr, "eth")
+                .unwrap()
+                .amount
+                .u128(),
+            1
+        );
+
+        assert_eq!(
+            app.wrap()
+                .query_balance("admin1", "eth")
+                .unwrap()
+                .amount
+                .u128(),
+            2
+        );
+
+        assert_eq!(
+            app.wrap()
+                .query_balance("admin2", "eth")
+                .unwrap()
+                .amount
+                .u128(),
+            2
+        );
+    }
 
     #[test]
     fn add_members() {
@@ -138,6 +224,7 @@ mod tests {
                 Addr::unchecked("owner"),
                 &InstantiateMsg {
                     admins: vec!["owner".to_owned()],
+                    donation_denom: "eth".to_owned(),
                 },
                 &[],
                 "Contract",
@@ -204,7 +291,10 @@ mod tests {
             .instantiate_contract(
                 code_id,
                 Addr::unchecked("owner"),
-                &InstantiateMsg { admins: vec![] },
+                &InstantiateMsg {
+                    admins: vec![],
+                    donation_denom: "eth".to_owned(),
+                },
                 &[],
                 "Contract",
                 None,
@@ -241,7 +331,10 @@ mod tests {
             .instantiate_contract(
                 code_id,
                 Addr::unchecked("owner"),
-                &InstantiateMsg { admins: vec![] },
+                &InstantiateMsg {
+                    admins: vec![],
+                    donation_denom: "eth".to_owned(),
+                },
                 &[],
                 "Contract",
                 None,
@@ -261,6 +354,7 @@ mod tests {
                 Addr::unchecked("owner"),
                 &InstantiateMsg {
                     admins: vec!["admin1".to_owned(), "admin2".to_owned()],
+                    donation_denom: "eth".to_owned(),
                 },
                 &[],
                 "Contract 2",
@@ -292,7 +386,10 @@ mod tests {
             .instantiate_contract(
                 code_id,
                 Addr::unchecked("owner"),
-                &InstantiateMsg { admins: vec![] },
+                &InstantiateMsg {
+                    admins: vec![],
+                    donation_denom: "eth".to_owned(),
+                },
                 &[],
                 "Contract",
                 None,
--- src/msg.rs
+++ src/msg.rs
@@ -20,10 +20,12 @@ pub enum QueryMsg {
 #[derive(Serialize, Deserialize, PartialEq, Debug, Clone)]
 pub struct InstantiateMsg {
     pub admins: Vec<String>,
+    pub donation_denom: String,
 }
 
 #[derive(Serialize, Deserialize, PartialEq, Debug, Clone)]
 pub enum ExecuteMsg {
     AddMembers { admins: Vec<String> },
     Leave {},
+    Donate {},
 }
